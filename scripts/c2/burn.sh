5866a4240e2789abe21a41c985d1d3979988893d
Carl Karsten <carl@personnelware.com>
2018-07-19 12:54:18 -0500

    C2s imaged, so I guess this works.

---
+++ scripts/c2/burn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+
+# do this better
+for file in /media/juser/*; do
+	echo umount "$file"
+done
+sync
+
+img=ubuntu-18.04-3.16-minimal-odroid-c2-20180626.img.xz
+dev=sdd
+
+wget -N https://odroid.in/ubuntu_18.04lts/${img}
+wget -N https://odroid.in/ubuntu_18.04lts/${img}.md5sum
+md5sum --check ${img}.md5sum
+
+xz --decompress --stdout ${img}|sudo dcfldd status=on bs=4M of=/dev/$dev
+
+sync
+sleep 5
+sync
+
--- scripts/c2/mk_c2b.sh
+++ scripts/c2/mk_c2b.sh
@@ -4,11 +4,15 @@
 
 apt-get --assume-yes install software-properties-common
 apt-add-repository --yes ppa:timvideos/fpga-support
-apt-bet --assume-yes install flterm fxload hdmi2usb-mode-switch hdmi2usb-udev python python3-pkg-resources usbutils vizzini-dkms voctomix-outcasts
+apt-get --assume-yes install \
+    tio flterm fxload hdmi2usb-mode-switch hdmi2usb-udev \
+    python python3-pkg-resources \
+    usbutils vizzini-dkms voctomix-outcasts
 
 cat > /lib/udev/rules.d/85-local-atlys-fx2-firmware.rules <<EOF
 # Load the hdmi2usb firmware onto the Atlys FX2 chip
 SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="1443", ATTRS{idProduct}=="0007", RUN+="/sbin/fxload -D \$tempnode -t fx2lp -I /lib/firmware/hdmi2usb.hex"
 EOF
 touch /forcefsck
+ip link show eth0
 reboot
