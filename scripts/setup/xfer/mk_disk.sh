8e60b2a1c4f77bd18daa1dfb2867735f2ccab7a8
Carl Karsten <carl@personnelware.com>
2024-07-06 13:32:01 -0500

    setup a removable disk for moving video files from voctomix box to veyepar processing.

---
+++ scripts/setup/xfer/mk_disk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+# device removable media happens to be plugged into
+# like /dev/sdc1
+dev=$1
+
+sudo apt install e2fsprogs pmount
+sudo e2label /dev/sdc1 xfer
+pmount /dev/disk/by-label/xfer xfer
+mkdir -p /media/xfer/Videos/veyepar
+pumount xfer
