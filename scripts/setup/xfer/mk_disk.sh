445a26c1068c03d42dd49ca93f53a93e970b009d
Carl Karsten <carl@personnelware.com>
2024-07-06 20:24:39 -0500

    hard code pyohio dirs because rsync isn't making them.

--- scripts/setup/xfer/mk_disk.sh
+++ scripts/setup/xfer/mk_disk.sh
@@ -7,5 +7,5 @@ dev=$1
 sudo apt install e2fsprogs pmount
 sudo e2label /dev/sdc1 xfer
 pmount /dev/disk/by-label/xfer xfer
-mkdir -p /media/xfer/Videos/veyepar
+mkdir -p /media/xfer/Videos/veyepar/PyOhio/PyOhio_2024/dv
 pumount xfer
