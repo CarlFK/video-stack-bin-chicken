2a4eac7b52b96b26c8acd4350f523e487737a40c
Carl Karsten <carl@personnelware.com>
2025-05-10 16:50:50 -0500

    rename master to main

--- scripts/setup/lc2/setup_lc2.sh
+++ scripts/setup/lc2/setup_lc2.sh
@@ -11,7 +11,7 @@
 mkdir scripts
 cd scripts
 
-stock=https://salsa.debian.org/debconf-video-team/ansible/raw/master/roles/pxe/tftp-server/files/scripts
+stock=https://salsa.debian.org/debconf-video-team/ansible/raw/main/roles/pxe/tftp-server/files/scripts
 for fn in ansible-firstboot.service  ansible-firstboot.sh  ansible-up late_command.cfg
 do
   wget ${stock}/${fn}
